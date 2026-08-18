a140eb298359e4413c6fae5ca55fa489de26b250
nav-uue <297357704+nav-uue@users.noreply.github.com>
2026-08-19 01:35:11 +0300

    feat: fetch disk storage metriks

--- src/metrics/hardware.rs
+++ src/metrics/hardware.rs
@@ -1,6 +1,7 @@
-use sysinfo::{Components, Disk, System};
+use sysinfo::{Components, Disks, System};
 use serde::Serialize;
 use std::collections::HashMap;
+use std::path::Path;
 
 
 #[derive(Debug, Serialize)]
@@ -13,7 +14,8 @@ pub struct HardwareInfo {
     total_swap: Vec<String>,
     #[serde(rename = "Used Swap")]
     used_swap: Vec<String>,
-    cpus: HashMap<String, Vec<String>>
+    cpus: HashMap<String, Vec<String>>,
+    storage: HashMap<String, Vec<String>>
 }
 
 
@@ -25,20 +27,48 @@ pub fn collect() -> HardwareInfo {
     let mut sys = System::new_all();
     sys.refresh_all();
     
+    // RAM and SWAP info
     let total_memory = format!("{}", sys.total_memory());
     let used_memory = format!("{}", sys.used_memory());
     let total_swap = format!("{}", sys.total_swap());
     let used_swap = format!("{}", sys.used_swap());
 
+    // CPU info
     for cpu in sys.cpus().iter() {
         
-        let cpu_name = format!("Name: '{}'", cpu.name());
+        let cpu_name = format!("{}", cpu.name());
         let cpu_usage = format!("Usage: {:.2}%", cpu.cpu_usage());
         let cpu_frequency = format!("Freq: {} MHz", cpu.frequency());
 
         let metrics_vector = vec![cpu_usage, cpu_frequency];
 
-        cpu_map.insert(cpu_name.clone().to_string(), metrics_vector);
+        cpu_map.insert(cpu_name, metrics_vector);
+
+    }
+
+    // Storage disks
+    let mut disk_map = HashMap::new();
+
+    let disks = Disks::new_with_refreshed_list();
+
+    for disk in &disks {
+
+        let disk_path = Path::new(disk.name());
+        let disk_name: String = disk_path
+            .file_name()
+            .map(|name| name.to_string_lossy().into_owned())
+            .unwrap_or_else(|| String::from("Unknown disk"));
+
+        let disk_type = format!("Type: {}", disk.kind());
+        let disk_fs = format!("Filesystem: {}", disk.file_system().to_string_lossy().into_owned());
+        let disk_mp = format!("Mount point: {}", disk.mount_point().to_string_lossy().into_owned());
+
+        let total_space = format!("Total space: {}", disk.total_space());
+        let avalible_space = format!("Avalible space: {}", disk.available_space());
+
+        let metrics_vector = vec![disk_type, disk_fs, disk_mp, total_space, avalible_space];
+
+        disk_map.insert(disk_name, metrics_vector);
 
     }
 
@@ -47,7 +77,8 @@ pub fn collect() -> HardwareInfo {
         used_memory: vec![used_memory],
         total_swap: vec![total_swap],
         used_swap: vec![used_swap],
-        cpus: cpu_map
+        cpus: cpu_map,
+        storage: disk_map
     }
 
 }
